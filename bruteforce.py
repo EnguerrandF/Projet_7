49df981d9134acd25180171aecf2bb2ff1071bce
EnguerrandF <feignon.enguerrand@outlook.fr>
2023-02-14 18:25:55 +0100

    bruteforce

---
+++ bruteforce.py
@@ -0,0 +1,65 @@
+ACTIONS = {
+    'Action-1': {'prix': 20, 'pourcentage': 5},
+    'Action-2': {'prix': 30, 'pourcentage': 10},
+    'Action-3': {'prix': 50, 'pourcentage': 15},
+    'Action-4': {'prix': 70, 'pourcentage': 20},
+    'Action-5': {'prix': 60, 'pourcentage': 17},
+    'Action-6': {'prix': 80, 'pourcentage': 25},
+    'Action-7': {'prix': 22, 'pourcentage': 7},
+    'Action-8': {'prix': 26, 'pourcentage': 11},
+    'Action-9': {'prix': 48, 'pourcentage': 13},
+    'Action-10': {'prix': 34, 'pourcentage': 27},
+    'Action-11': {'prix': 42, 'pourcentage': 17},
+    'Action-12': {'prix': 110, 'pourcentage': 9},
+    'Action-13': {'prix': 38, 'pourcentage': 23},
+    'Action-14': {'prix': 14, 'pourcentage': 1},
+    'Action-15': {'prix': 18, 'pourcentage': 3},
+    'Action-16': {'prix': 8, 'pourcentage': 8},
+    'Action-17': {'prix': 4, 'pourcentage': 12},
+    'Action-18': {'prix': 10, 'pourcentage': 14},
+    'Action-19': {'prix': 24, 'pourcentage': 21},
+    'Action-20': {'prix': 114, 'pourcentage': 18},
+}
+
+
+class BestActionsForThePrice:
+    def __init__(self, dictionnaire_action):
+        self.dictionnaire_action = dictionnaire_action
+        self.max_spend = 500
+        self.dic_result_profitability = {}
+        self.list_selection_action = []
+
+    def main(self):
+        self.check_action()
+        self.action_to_select()
+        print(self.calculate_total_action_by())
+
+    def check_action(self):
+        for action in self.dictionnaire_action:
+            result = self.calculate_action(self.dictionnaire_action[action]["prix"],
+                                           self.dictionnaire_action[action]["pourcentage"])
+            self.dic_result_profitability[action] = result
+
+        self.dic_result_profitability = dict(sorted(self.dic_result_profitability.items(),
+                                                    key=lambda x: x[1], reverse=True))
+
+    def calculate_action(self, prix, pourcentage):
+        return ((prix * pourcentage) / 100)
+
+    def action_to_select(self):
+        for action in self.dic_result_profitability:
+            if (self.max_spend - self.dictionnaire_action[action]["prix"]) < 0:
+                pass
+            else:
+                self.max_spend = self.max_spend - self.dictionnaire_action[action]["prix"]
+                self.list_selection_action.append(action)
+
+    def calculate_total_action_by(self):
+        result = 0
+        for action in self.list_selection_action:
+            print("", action, "prix action : ", self.dictionnaire_action[action]["prix"])
+            result = self.dictionnaire_action[action]["prix"] + result
+        return result
+
+
+BestActionsForThePrice(ACTIONS).main()
